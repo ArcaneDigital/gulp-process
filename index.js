0644aeb63bba74d9999973cde0fd103bccba257b
Jamieson Roberts <hello@jamiesonroberts.com>
2018-11-09 08:18:19 -0500

    Half finished thought

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
 import files from './tasks/files';
 
+gulp.task('files', files);
+
 const build = gulp.series(files('./test/src', './test/dist'));
 gulp.task('build', build);
 
--- tasks/files.js
+++ tasks/files.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 
 const files = (input, output) => gulp.src(input).pipe(gulp.dest(output));
-gulp.task('files', files);
 
 export default files;
