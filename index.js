399885b4cfdf096711f32a38956d3fe5822b3d44
Jamieson Roberts <hello@jamiesonroberts.com>
2018-11-12 01:01:57 -0500

    Feat: building out the style task and figuring out specific undocumented features of Gulp4

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-const { src, dest, series, parallel } = require('gulp');
+// Global dependencies
+const { src, dest, series, parallel, lastRun, task } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 
-const global = {};
+// Style dependencies
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const postcss = require('gulp-postcss');
+const autoPrefixer = require('autoprefixer');
+const cssNano = require('cssnano');
+
+// Notifications
+const notifier = require('node-notifier');
 
 const env = process.env.NODE_ENV || 'production';
 
+const global = {};
 const config = options => {
-    global.src = options.src;
-    global.out = options.out;
+    global.src = options.source;
+    global.out = options.destination;
 };
 
-const fileTask = () => src(global.src.file).pipe(dest(global.out.file));
+const notification = () => {};
 
-const serialBuild = series(fileTask);
-const build = parallel(fileTask);
+const style = () => {
+    const processors = [autoPrefixer];
+    if (env === 'production') {
+        processors.push(cssNano({ discardUnused: { fontFace: false } }));
+    }
+
+    let stream = src(global.src.style, { since: lastRun('style') });
+
+    if (env !== 'production') {
+        stream = stream.pipe(sourcemaps.init());
+    }
+
+    stream = stream
+        .pipe(sassGlob())
+        .pipe(sass({ includePaths: ['./node_modules/**/'] }))
+        .on('error', err => {
+            notification('styles', err);
+            stream.emit('end');
+        })
+        .pipe(postcss(processors));
+
+    if (env !== 'production') {
+        stream = stream.pipe(sourcemaps.write('.'));
+    }
+
+    return stream.pipe(dest(global.out.style));
+};
+task('style', style);
+
+const files = () => {
+    const stream = src(global.src.file)
+        .on('error', err => {
+            notification('files', err);
+            stream.emit('end');
+        })
+        .pipe(dest(global.out.file));
+    return stream;
+};
+
+const watcher = () => {};
+
+const build = series(files, style);
 
 module.exports = {
     default: build,
-    serialBuild,
     config,
 };
